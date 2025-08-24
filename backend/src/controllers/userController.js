fbbd9d6cfddc07db96fcfae3e1add737f3397bda
triggereddown <deepmoitra2@gmail.com>
2025-08-24 13:48:36 +0530

    Implemented Logout functionality ,login and signup

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,33 +1,29 @@
 const User = require("../models/userModel.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
 
-    // Validation for all fields
     if (!fullName || !username || !password || !confirmPassword || !gender) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    // Validation for password
     if (password !== confirmPassword) {
       return res
         .status(400)
         .json({ message: "Password and Confirm Password doesn't match" });
     }
 
-    // Check for user
     const user = await User.findOne({ username });
 
     if (user) {
       return res.status(400).json({ message: "User Already Exists" });
     }
 
-    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Save user
     await User.create({
       fullName,
       username,
@@ -42,8 +38,60 @@ const register = async (req, res) => {
   }
 };
 
-// Export CommonJS style
-module.exports = register;
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalid Credentials", success: false });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid Credentials", success: false });
+    }
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
 
+    return res
+      .status(200)
+      .cookie("token", {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        // to ensure the http security from cyber cookie stealing
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        _id: user._id,
+        username: user.username,
+        fullName: user.fullName,
+      });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
 
+// Not using await since Logout doesn't requires any operation which shall take time
+const logout = (req, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "User successfully logged out",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
 
+module.exports = { register, login, logout };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const register = require("../controllers/userController.js");
+const { register, login, logout } = require("../controllers/userController.js");
 
 const router = express.Router();
 
 router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
 
 module.exports = router;

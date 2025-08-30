201fc1296df441fa86b6311334e0d68b56283b71
triggereddown <deepmoitra2@gmail.com>
2025-08-30 19:03:18 +0530

    Html for signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form action="">
+        <div>
+          <label htmlFor="">
+            <span>Full name</span>
+          </label>
+          <input type="text" placeholder="Enter Full Name" />
+        </div>
+        <div>
+          <label htmlFor="">
+            <span>User name</span>
+          </label>
+          <input type="text" placeholder="Enter UserName" />
+        </div>
+        <div>
+          <label htmlFor="">
+            <span>Password</span>
+          </label>
+          <input type="password" placeholder="Enter Password" />
+        </div>
+        <div>
+          <label htmlFor="">
+            <span>Confirm Password</span>
+          </label>
+          <input type="password" placeholder="Enter Password" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;

b7153ca854b415a435badc5d9a187a3199b0ce72
triggereddown <deepmoitra2@gmail.com>
2025-08-31 20:36:25 +0530

    Login SignUp Frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  // e because e will take care to change when the input is by user on form
+
+  const onSubmitHandler = (e) => {
+    // To stop reloading the page on input
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      username: "",
+
+      password: "",
+    });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={onSubmitHandler} action="">
+        <div>
+          <label htmlFor="">
+            <span>User name</span>
+          </label>
+          <input
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            type="text"
+            placeholder="Enter UserName"
+          />
+        </div>
+        <div>
+          <label htmlFor="">
+            <span>Password</span>
+          </label>
+          <input
+            value={user.password}
+            // ... is spread operator to get the existing value
+            // the onbject of setuser is destructured as any props to access the inside elements
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            type="password"
+            placeholder="Enter Password"
+          />
+        </div>
+        <div>
+          <Link to="/signup">Don't have an account? Sign Up</Link>
+          <div>
+            <button>Login</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,40 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  // makin an object to get the input
+  const [user, setUser] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  // e because e will take care to change when the input is by user on form
+
+  const onSubmitHandler = (e) => {
+    // To stop reloading the page on input
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      username: "",
+      fullName: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
   return (
     <div>
       <h2>Sign Up</h2>
-      <form action="">
+      <form
+        // Calling the onsubmit handler function with usestate on clicking on submit button
+        onSubmit={onSubmitHandler}
+        action=""
+      >
         <div>
           <label htmlFor="">
             <span>Full name</span>
           </label>
-          <input type="text" placeholder="Enter Full Name" />
+          <input
+            value={user.fullName}
+            // to get the existing users and also setting the fullname as e.target .vqalue
+            onChange={(e) => setUser({ ...user, fullName: e.target.value })}
+            type="text"
+            placeholder="Enter Full Name"
+          />
         </div>
         <div>
           <label htmlFor="">
             <span>User name</span>
           </label>
-          <input type="text" placeholder="Enter UserName" />
+          <input
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            type="text"
+            placeholder="Enter UserName"
+          />
         </div>
         <div>
           <label htmlFor="">
             <span>Password</span>
           </label>
-          <input type="password" placeholder="Enter Password" />
+          <input
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            type="password"
+            placeholder="Enter Password"
+          />
         </div>
         <div>
           <label htmlFor="">
             <span>Confirm Password</span>
           </label>
-          <input type="password" placeholder="Enter Password" />
+          <input
+            value={user.confirmPassword}
+            onChange={(e) =>
+              setUser({ ...user, confirmPassword: e.target.value })
+            }
+            type="password"
+            placeholder="Enter Password"
+          />
           <div>
             <input type="checkbox" />
           </div>
           <Link to="/login">Already have an account? Login ?</Link>
           <div>
-            <button>Sign Up</button>
+            <button type="submit">Sign Up</button>
           </div>
         </div>
       </form>

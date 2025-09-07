252b5c050778d139d7b2a29df82d69d00a25e4ea
triggereddown <deepmoitra2@gmail.com>
2025-09-07 20:22:37 +0530

    Added middleware and updated send msg route

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -1,12 +1,43 @@
+const Conversation = require("../models/conversationModel.js");
+const Message = require("../models/messageModel.js");
+
 const sendMessage = async (req, res) => {
   try {
-    //Since we know that in auth we have saved id of looged in user so we will get it from there
+    //Since we know that in auth we have saved id of logged in user so we will get it from there
 
     const senderId = req.id;
 
     const receiverId = req.params.id;
     // So now wqe have the id of sender as well as receiver ^^
     const { message } = req.body;
+    //We declare a variabe tp check how many conversations are going on and among them do we have sender and receiverid as participants or not
+    let gotConversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    // Its like checking the register that if conversation doesn't exists then in the db we will create one between two
+    if (!gotConversation) {
+      gotConversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    //So the message is created here aS we have passed the required 3 entries one by one
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      gotConversation.messages.push(newMessage._id);
+    }
+
+    await gotConversation.save();
+    return res.status(200).json({
+      success: true,
+      message: "Message Sent",
+    });
   } catch (err) {
     console.log(err);
     return res

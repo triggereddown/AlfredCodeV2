1deca29c69272c1f4999ec1dd70c1597f7fd5478
triggereddown <deepmoitra2@gmail.com>
2025-10-23 18:39:24 +0530

    Fetched Data to show the present user on the      database

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./OtherUsers.css";
 import OtherUser from "./OtherUser";
+import useGetOtherUsers from "../hooks/useGetOtherUsers";
 
 const OtherUsers = () => {
+  useGetOtherUsers();
+
   return <OtherUser />;
 };
 
--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -5,6 +5,7 @@ const useGetOtherUsers = () => {
   useEffect(() => {
     const fetchOtherUsers = async () => {
       try {
+        axios.defaults.withCredentials = true;
         const res = await axios.get("http://localhost:3000/api/v1/user/chat");
         console.log(res);
       } catch (error) {

70d54673d448ec2f719d253f6df13c919eaac634
triggereddown <deepmoitra2@gmail.com>
2025-10-31 13:56:40 +0530

    Updated a typo in login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setAuthUser } from "../redux/userSlice.js";
-import "./login.css";
+import "./Login.css";
 
 const Login = () => {
   const [user, setUser] = useState({

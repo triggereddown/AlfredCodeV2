f8e49e394bcb0caf0fe040f677714794962b94c0
triggereddown <deepmoitra2@gmail.com>
2025-10-31 15:13:39 +0530

    Updated the code review

--- frontend/src/components/Review.jsx
+++ frontend/src/components/Review.jsx
@@ -23,9 +23,12 @@ const Review = () => {
 
   async function reviewCode() {
     try {
-      const response = await axios.post("http://localhost:3000/ai/get-review", {
-        code,
-      });
+      const response = await axios.post(
+        "https://alfredchatv2backend.onrender.com/ai/get-review",
+        {
+          code,
+        }
+      );
       setReview(response.data);
     } catch (error) {
       console.error("Error fetching review:", error);
